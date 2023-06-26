7d22997e44aa3864677110763b6b999b74a376d6
Leop333 <42681924+Leop333@users.noreply.github.com>
2023-06-26 21:52:50 +0300

    Add files via upload
    R code to creat mail corpus from .pst file ( export from outlook)

---
+++ R pst to df.R
@@ -0,0 +1,37 @@
+#remotes::install_github("hrbrmstr/freepst")
+
+library(freepst)
+setwd("D:/Study/CL/prj2")
+mymail <- read_pst("my_em.pst")
+mailshort <- mymail[,c(1,2,12,13)]
+names(mymail)
+
+length(unique(mymail$subject))
+#psych::describe(mymail)
+write.csv2(mailshort,"mail_fromr.csv")
+#explore
+table(mailshort$folder)
+table(mailshort$sent_by)
+table(mailshort$subject)
+length(unique(mailshort$subject))
+length(unique(mailshort$sent_by))
+
+#clean 2
+which(length(mailshort[3,4])<10)
+
+#clean
+length(mailshort[3,4])
+length(mailshort[,4])
+which(length(mailshort[3,4])<10)
+small <- which(mailshort$body<50)
+mail_shrt_cln<- mailshort[-small,]
+#clean emptys
+table(mail_shrt_cln$folder)
+which(mail_shrt_cln$folder!="Входящие")
+
+#write
+write.csv2(mail_shrt_cln,"mail_fromr_clean.csv",row.names=FALSE)
+  length(unique(mail_shrt_dirt$subject))
+length(unique(mail_shrt_cln$subject))
+write.table(mail_shrt_cln,file = "mail_fromr_clean.txt",dec = ".",sep = ";;;",quote = FALSE)
+
